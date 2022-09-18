62b908712e45a1ee69225324b74ecb9cf29ba3c6
sajhao <113296628+sajhao@users.noreply.github.com>
2022-09-18 23:24:30 +0800

    Delete app1 directory

--- app1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class app1Config(AppConfig):
-    name = 'app1'
--- app1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'app1'  # declaring the app_name here
-
-urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('second/', views.secondpage, name='secondpage'),
-]
